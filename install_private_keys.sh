cc3694ed201f6da115d8f46bad88da3fd0debc52
Jonas <Jonas@Hess.pm>
2021-08-24 22:32:25 +0200

    file permissions

--- install_private_keys.sh
+++ install_private_keys.sh
@@ -6,7 +6,26 @@ cd $BASEDIR
 mkdir -p ~/.ssh/
 #chmod 600 ~/.ssh/authorized_keys
 
-./keys/decrypt.sh ./keys/id_rsa_JH_Github.encrypted ~/.ssh/id_rsa_JH_Github
-chmod 600 ~/.ssh/id_rsa_JH_Github
+file=~/.ssh/id_rsa_JH_Github
+
+if [ -f "$file" ] ; then
+    rm "$file"
+fi
+
+./keys/decrypt.sh ./keys/id_rsa_JH_Github.encrypted $file
+
+
+# Setup Github
+chmod -R 700 ~/.ssh/*
+touch ~/.ssh/known_hosts
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+chmod -R 400 ~/.ssh/*
 chmod 700 ~/.ssh/
+
+
+read -p "Enter Your Name: " name
+read -p "Enter Your E-Mail: " email
+
+git config --global user.name $name
+git config --global user.email $email
 
