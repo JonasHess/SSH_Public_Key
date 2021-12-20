ffd22424df6069899a14ef4de7fb5f9cd472beff
JonasHess <JonasHess@users.noreply.github.com>
2021-12-20 23:09:26 +0100

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+sh sudo apt update 
+sudo apt install git -y 
+git clone https://github.com/JonasHess/SSH_Public_Key.git
+cd SSH_Public_Key 
+chmod +x ./*.sh 
+./install_public_keys.sh 
+./install_private_keys.sh 
+git remote set-url origin git@github.com:JonasHess/SSH_Public_Key.git
