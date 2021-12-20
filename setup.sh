c78ec15028542f0b2ee8472adf30a60bff70f473
JonasHess <JonasHess@users.noreply.github.com>
2021-12-20 23:13:24 +0100

    setup.sh non sudo

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sh sudo apt update 
-sudo apt install git -y 
+#sh sudo apt update 
+#sudo apt install git -y 
 git clone https://github.com/JonasHess/SSH_Public_Key.git
 cd SSH_Public_Key 
 chmod +x ./*.sh 
